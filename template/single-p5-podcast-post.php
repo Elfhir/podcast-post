4d64a9d398a3a4d2fcb12b43235fd14e31b05de6
Elfhir <ta.thanhliem@gmail.com>
2013-11-29 02:15:09 +0100

    Begin to print custom fields

--- template/single-p5-podcast-post.php
+++ template/single-p5-podcast-post.php
@@ -5,20 +5,40 @@ Template Name Posts: single-p5-podcast-post
 ?>
 <?php get_header(); ?>
 <div class="main p5-podcast-posts">
-	<h1><?php the_title(); ?></h1>
-	<h2> Canard </h2>
 	<?php wp_reset_postdata(); ?>
-	<?php query_posts('posts_per_page=-1&post_type=p5-podcast-post'); ?>
 
 	<?php if (have_posts()) : ?>
 		<?php while (have_posts()) : the_post(); ?>
-			<div class="p5-podcast-post">
+			<div id="post-<?php the_ID(); ?>" class="p5-podcast-post">
 
-				<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php //get_template_part( 'content', get_post_format() ); ?>
 
-				<h3 class="p5-podcast-post-name"><?php the_title(); ?></h3>
-				<p class="p5-podcast-post-description"><?php the_excerpt(); ?></p>
-				<?php the_post_thumbnail(); ?>
+				<h1 class="p5-podcast-post-name">Title : <?php the_title(); ?></h1>
+				<p class="p5-podcast-post-description">Excerpt : <?php the_excerpt(); ?></p>
+				<ul>
+					<li>desc : <?php the_excerpt(); ?></li>
+					<li>Image : <?php the_post_thumbnail(); ?></li>
+					<?php 
+						
+						$custom = get_post_custom(the_ID());
+						foreach ( $custom as $key => $value ) {
+							if($key === "mp3") {
+    							echo "<li>". $key . " => " . $value[0] . "<li />";
+							}
+							elseif($key === "duration") {
+    							echo "<li>". $key . " => " . $value[0] . "<li />";
+							}
+							elseif($key === "subtitle") {
+    							echo "<li>". $key . " => " . $value[0] . "<li />";
+							}
+							elseif($key === "order") {
+    							echo "<li>". $key . " => " . $value[0] . "<li />";
+							}
+    					}
+
+   					?>
+				</ul>
+				<p class="postmetadata">Posted in <?php the_category(', '); ?></p>
 	    	</div>
 	    <?php endwhile; ?>
   <?php endif; ?>
